c72320ce15164d66061ecf5b130233bcb24db448
William Shepherd <will@codeincolor.io>
2021-12-17 12:19:10 -0600

    Use the plugin on a page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// eslint-disable-next-line no-unused-vars
+import OneSignal from "onesignal-vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  beforeCreate() {
+    this.$OneSignal.showSlidedownPrompt();
+  },
+};
 </script>
 
 <style>
