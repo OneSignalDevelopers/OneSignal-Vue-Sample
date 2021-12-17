7f678a567d5542484af8e4a2dc926b60e980de1b
William Shepherd <will@codeincolor.io>
2021-12-17 12:19:01 -0600

    Initialize the plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import OneSignal from "onesignal-vue";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
+
+Vue.use(OneSignal);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  beforeMount() {
+    this.$OneSignal.init({
+      appId: "<You OneSignal application identifier",
+      allowLocalhostAsSecureOrigin: true,
+    });
+  },
+}).$mount("#app");
